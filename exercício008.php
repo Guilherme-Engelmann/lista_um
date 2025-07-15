25128fba1a4bb2e9fdf82a7af6d88ea493687d68
Guilherme-Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-07-15 12:18:33 -0300

    add

---
+++ exercício008.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <form method="POST" action="">
+
+        <label for="numero_par">Digite um Número</label>
+        <input type="number" id="numero_par" name="numero_par" required>
+        <button type="submit" name="Verificar_numero">Verificar</button>
+
+    </form>
+</body>
+</html>
---
+++ exercício014.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><form method="POST" action="">
+    
+        <label for="fatorial">Verificar número</label>
+        <input type="number" id="fatorial" name="fatorial" required>
+        <button type="submit" name="v_fatorial">Verificar</button>
+
+    </form>
+    
+</body>
+</html>
